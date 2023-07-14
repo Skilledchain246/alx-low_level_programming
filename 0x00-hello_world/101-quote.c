b1cf2ca114b0d7f4063b9fac1b98a1116b20f846
Thabiso <Nehemiahmhulatsi@outlook.com>
2023-07-13 18:19:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio>
-#include <unstd.h>
+#include <unistd.h>
 
 /**
- * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * main - UNIX is basically
  *
  * Return: Always 1 (Success)
  *
  */
 int main(void)
 {
-	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
