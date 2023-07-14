0e97eec02684c80fee8dd995df2a88e43aa8eb1d
Thabiso <Nehemiahmhulatsi@outlook.com>
2023-07-13 18:22:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
  */
 int main(void)
 {
-puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
