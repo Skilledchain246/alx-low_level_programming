afaa31b2c82e9fd959088dd4d83161e3e2b162db
Thabiso <Nehemiahmhulatsi@outlook.com>
2023-07-19 01:36:20 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,19 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int main(void)
+{
+	int i, sum = 0;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
-		}
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
-	sum = sum3 + sum5;
+
 	printf("%d\n", sum);
+
 	return (0);
+}
+
 }
