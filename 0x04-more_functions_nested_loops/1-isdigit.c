4f45af817dcbb2969026f1f362571eb6886853df
Thabiso <Nehemiahmhulatsi@outlook.com>
2023-07-20 06:54:07 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit
+ * @c: int type
+ * Return: 1 if gitgits or 0
+ */
+int _isdigit(int c)
+{
+	if (c > 46 && c < 58)
+		return (1);
+	else
+		return (0);
+}
